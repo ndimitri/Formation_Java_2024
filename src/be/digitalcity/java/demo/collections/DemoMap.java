41393ab03878a0b00f4fc06612d303b2d681a4ee
Nicolas <dimitriadisnicolas@gmail.com>
2024-07-26 09:05:25 +0200

    change: optimisation for some tab code

--- src/be/digitalcity/java/demo/collections/DemoMap.java
+++ src/be/digitalcity/java/demo/collections/DemoMap.java
@@ -3,24 +3,25 @@ package be.digitalcity.java.demo.collections;
 import java.util.HashMap;
 
 public class DemoMap {
-  public static void main(String[] args) {
 
-    HashMap<String, int[]> notesParClasse = new HashMap<>();
+    public static void main(String[] args) {
 
-    // Ajouter ou Remplacer
-    notesParClasse.put( "A1", new int[]{1,2,3} );
-    if( notesParClasse.containsKey("C4") ){
-      // remplacement
-      notesParClasse.put( "C4", new int[]{20} );
+        HashMap<String, int[]> notesParClasse = new HashMap<>();
+
+        // Ajouter ou Remplacer
+        notesParClasse.put( "A1", new int[]{1,2,3} );
+        if( notesParClasse.containsKey("C4") ){
+            // remplacement
+            notesParClasse.put( "C4", new int[]{20} );
+        }
+        else {
+            // ajout
+            notesParClasse.put( "C4", new int[]{10,2,0,5} );
+        }
+
+
+
+
     }
-    else {
-      // ajout
-      notesParClasse.put( "C4", new int[]{10,2,0,5} );
-    }
-
-
-
-
-  }
 
 }
--- src/be/digitalcity/java/demo/collections/DemoSet.java
+++ src/be/digitalcity/java/demo/collections/DemoSet.java
@@ -3,25 +3,27 @@ package be.digitalcity.java.demo.collections;
 import java.util.HashSet;
 
 public class DemoSet {
-  public static void main(String[] args) {
 
-    // Collection non ordonnée, ne permettant pas les doublons, sans accès direct
-    HashSet<String> prenoms = new HashSet<>();
+    public static void main(String[] args) {
 
-    // Ajouter
-    prenoms.add("Luc"); // N'ajoute que si aucun autre élément n'est ".equals()"
-    prenoms.add("Marie");
-    prenoms.add("Marie"); // ne sera pas ajouté
+        // Collection non ordonnée, ne permettant pas les doublons, sans accès direct
+        HashSet<String> prenoms = new HashSet<>();
 
-    // Recupérer
-    // Impossible sans iterator ou foreach
-    for (String prenom : prenoms) {
-      System.out.println(prenom);
+        // Ajouter
+        prenoms.add("Luc"); // N'ajoute que si aucun autre élément n'est ".equals()"
+        prenoms.add("Marie");
+        prenoms.add("Marie"); // ne sera pas ajouté
+
+        // Recupérer
+        // Impossible sans iterator ou foreach
+        for (String prenom : prenoms) {
+            System.out.println(prenom);
+        }
+
+        // Retirer
+        prenoms.remove("Marie"); // via ".equals()"
+
     }
 
-    // Retirer
-    prenoms.remove("Marie"); // via ".equals()"
-
-  }
 
 }
