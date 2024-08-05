c3a47a4ecb11de8809cb84dabbca1da23afc9399
Nicolas <dimitriadisnicolas@gmail.com>
2024-08-05 15:58:31 +0200

    add: add Exo Static et enum

---
+++ src/be/digitalcity/java/exo/oo/lanceDes/LancerDes.java
@@ -0,0 +1,13 @@
+package be.digitalcity.java.exo.oo.lanceDes;
+
+public enum LancerDes {
+    SIX(6),
+    DIX(10);
+
+    public int nbrFaces;
+
+    LancerDes(int nbrFaces){
+        this.nbrFaces = nbrFaces;
+    }
+
+}
