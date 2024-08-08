fdcbfbee4f19be7b70f1f37701d7666b0d07ab2a
Nicolas <dimitriadisnicolas@gmail.com>
2024-08-08 10:08:54 +0200

    add: add lamba function

--- src/be/digitalcity/java/exo/interfaceFonctionnelle/carwash/Carwash.java
+++ src/be/digitalcity/java/exo/interfaceFonctionnelle/carwash/Carwash.java
@@ -2,26 +2,31 @@ package be.digitalcity.java.exo.interfaceFonctionnelle.carwash;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Carwash {
 
-    private ActionVoiture preparer = (Voiture voiture) -> System.out.println("Je prépare la voiture : " + voiture.getPlaque());
-    private ActionVoiture laver = (Voiture voiture) -> System.out.println("Je lave la voiture : " + voiture.getPlaque());
-    private ActionVoiture secher = (Voiture voiture) -> System.out.println("Je sèche la voiture : " + voiture.getPlaque());
-    private ActionVoiture finaliser = (Voiture voiture) -> System.out.println("Je finalise la voiture : " + voiture.getPlaque());
-
-
-    List<ActionVoiture> evenementsVoiture = List.of(preparer, laver, secher, finaliser);
-
-
-    public void traiter(Voiture voiture){
-
-        List<ActionVoiture> evenementsVoiture2 = List.of(
+    //1
+    private List<ActionVoiture> evenementsVoiture2;
+    public Carwash(){
+        evenementsVoiture2 = List.of(
                 (Voiture v) -> System.out.println("Je prépare la voiture "+v.getPlaque()),
                 (Voiture v) -> System.out.println("Je lave la voiture "+v.getPlaque()),
                 (Voiture v) -> System.out.println("Je sèche la voiture "+v.getPlaque()),
                 (Voiture v) -> System.out.println("Je finalise la voiture "+v.getPlaque()));
 
+    }
+
+    //2
+    private ActionVoiture preparer = (Voiture voiture) -> System.out.println("Je prépare la voiture : " + voiture.getPlaque());
+    private ActionVoiture laver = (Voiture voiture) -> System.out.println("Je lave la voiture : " + voiture.getPlaque());
+    private ActionVoiture secher = (Voiture voiture) -> System.out.println("Je sèche la voiture : " + voiture.getPlaque());
+    private ActionVoiture finaliser = (Voiture voiture) -> System.out.println("Je finalise la voiture : " + voiture.getPlaque());
+
+    List<ActionVoiture> evenementsVoiture = List.of(preparer, laver, secher, finaliser);
+
+
+    public void traiter(Voiture voiture){
 //        for (ActionVoiture actionVoiture : evenementsVoiture) {
 //            actionVoiture.action(voiture);
 //        }
@@ -29,7 +34,6 @@ public class Carwash {
         for (ActionVoiture actionVoiture : evenementsVoiture2) {
             actionVoiture.action(voiture);
         }
-
 
 
 //      evenementsVoiture.forEach(actionVoiture -> actionVoiture.action(voiture));
